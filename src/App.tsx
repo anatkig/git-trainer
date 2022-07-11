572aa684811e0d89cce44909cfaf7868a97bfcd7
anatkig <keeggooggle@gmail.com>
2022-07-11 17:42:31 +0600

    continued developing question container

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Git Questions</header>
-      <QuestionContainer />
+      <QuestionContainer question={giveRandomQuestion()} />
       {giveRandomQuestion()}
     </div>
   );
--- src/components/question-container/QuestionContainer.tsx
+++ src/components/question-container/QuestionContainer.tsx
@@ -1,7 +1,24 @@
 import "./question-container.css";
 
-const QuestionContainer = () => {
-  return <div id="question-container"></div>;
+const QuestionContainer = ({ question }: { question: string }) => {
+  const questionArray = question.split("?");
+  const answersArray = questionArray[1].trim().split(" - ");
+  console.log(answersArray);
+
+  return (
+    <div id="question-container">
+      <div id="question">{questionArray[0] + "?"}</div>
+      <div id="answers">
+        <ul>
+          {answersArray.map((answer) => (
+            <li className="answer-option" key={answer}>
+              {answer}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default QuestionContainer;
