0475089b54a5ccdc59c34c04b300c101f75f6bf2
anatkig <keeggooggle@gmail.com>
2022-09-07 12:31:04 +0600

    added to rest api

--- src/questions/giveRandomRESTAPIQuestion.ts
+++ src/questions/giveRandomRESTAPIQuestion.ts
@@ -2668,6 +2668,70 @@ The Content-Type representation header is used to indicate the original media ty
 
 A MIME type (now properly called "media type", but also sometimes "content type") is a string sent along with a file indicating the type of the file (describing the content format, for example, a sound file might be labeled audio/ogg, or an image file image/png).
 @@
+#### Q226. What is described below?
+@@
+All API requests for the same resource should look the same, no matter where the request comes from. The REST API should ensure that the same piece of data, such as the name or email address of a user, belongs to only one uniform resource identifier (URI). Resources shouldn’t be too large but should contain every piece of information that the client might need.
+@@
+- [x] Uniform interface
+- [ ] Code on demand (optional)
+- [ ] Client-server decoupling
+- [ ] Cacheability
+@@
+
+
+#### Q227. What is described below?
+@@
+In REST&nbsp;API design, client and server applications must be completely independent of each other. The only information the client application should know is the URI of the requested resource; it can't interact with the server application in any other ways. Similarly, a server application shouldn't modify the client application other than passing it to the requested data via HTTP.
+@@
+- [x] Client-server decoupling
+- [ ] Statelessness
+- [ ] Uniform interface
+- [ ] Cacheability
+@@
+
+
+#### Q228. What is described below?
+@@
+REST APIs are stateless, meaning that each request needs to include all the information necessary for processing it. In other words, REST APIs do not require any server-side sessions. Server applications aren’t allowed to store any data related to a client request.
+@@
+- [x] Statelessness
+- [ ] Layered system architecture
+- [ ] Client-server decoupling
+- [ ] Cacheability
+@@
+
+
+#### Q229. What is described below?
+@@
+When possible, resources should be cacheable on the client or server side. Server responses also need to contain information about whether caching is allowed for the delivered resource. The goal is to improve performance on the client side, while increasing scalability on the server side.
+@@
+- [x] Cacheability
+- [ ] Uniform interface
+- [ ] Layered system architecture
+- [ ] Code on demand (optional)
+@@
+
+
+#### Q230. What is described below?
+@@
+In REST APIs, the calls and responses go through different layers. As a rule of thumb, don’t assume that the client and server applications connect directly to each other. There may be a number of different intermediaries in the communication loop. REST APIs need to be designed so that neither the client nor the server can tell whether it communicates with the end application or an intermediary.
+@@
+- [x] Layered system architecture
+- [ ] Code on demand (optional)
+- [ ] Uniform interface
+- [ ] Cacheability
+@@
+
+
+#### Q231. What is described below?
+@@
+REST APIs usually send static resources, but in certain cases, responses can also contain executable code (such as Java applets). In these cases, the code should only run on-demand.
+@@
+- [x] Code on demand (optional)
+- [ ] Client-server decoupling
+- [ ] Statelessness
+- [ ] Uniform interface
+@@
 `;
 
   const dataArray = data.split('####').slice(1);
